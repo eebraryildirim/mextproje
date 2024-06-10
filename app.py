b84b19155a7c2ba062dbfc40447a7f4f9c2d1504
eebraryildirim <ebraryildirim90@gmail.com>
2024-06-10 12:03:14 +0300

    s

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 import streamlit as st
+import pandas as pd
+import numpy as np
+from PIL import Image
+from PyPDF2 import PdfReader
+from pages_2.dataset import dataset_function
+from pdf2image import convert_from_path
+import io
+import matplotlib.pyplot as plt
+
 st.header("Uygulama v1")
